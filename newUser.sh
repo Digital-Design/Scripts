ce2c49c7907ea711aab261680b22fdc7f8e6c470
Paul <paul.du56@gmail.com>
2016-02-02 17:12:04 +0100

    update

--- newUser.sh
+++ newUser.sh
@@ -17,9 +17,50 @@ if id -u "$1" >/dev/null 2>&1; then
 	exit 1
 fi
 
+userDir='/home/'$1'/www/'
+
 echo "Creating user "$1"..."
 
-#useradd --create-home --skel=/etc/skel --shell=/bin/bash $1
+useradd --create-home --skel=/etc/skel --shell=/bin/bash $1
 
-mkdir -p /home/$1/www/
+mkdir -p $userDir
+
+read -p 'Clone git repo (optional): ' cloneUrl;
+
+if [ ! -z "$cloneUrl" ]
+then
+	git clone $cloneUrl $userDir
+else
+	echo $1 > $userdir/index.html
+fi
+
+servn='clubisen.fr'
+
+chown -R $1:www-data $userDir
+chmod -R '755' $userDir
+mkdir /var/log/apache2/$1/
+
+echo "#### Virtual Host pour "$1.$servn"
+
+<VirtualHost *:80>
+	ServerName $servn
+	ServerAlias $1.$servn
+	DocumentRoot $userDir
+
+	<Directory $userDir>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride All
+		Order allow,deny
+		allow from all
+	</Directory>
+	CustomLog /var/log/apache2/$1/access.log combined
+	ErrorLog /var/log/apache2/$1/error.log
+</VirtualHost>" > /etc/apache2/site-available/$1.$servn.conf
+
+if [ ! -f /etc/apache2/site-available/$1.$servn.conf ]
+then
+	echo "Virtual host wasn't created !"
+else
+	echo "Virtual host created !"
+fi
 
